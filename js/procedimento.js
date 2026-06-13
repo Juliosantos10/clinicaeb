77c03481d46d76287d60bfbee195eec1c1276690
julio <berlimbk@gmail.com>
2026-06-13 17:02:49 -0300

    add
    adicionado cards e novos procedimentos

--- js/procedimento.js
+++ js/procedimento.js
@@ -4,19 +4,61 @@ const procedimentos = {
     botox: {
         titulo: "Botox",
         descricao: "O Botox suaviza rugas e linhas de expressão, proporcionando aparência mais jovem e natural. Rápido, seguro e realizado por profissionais qualificados.",
-        imagem: "../img/botox.png"
+        imagem: "img/botox.png"
     },
 
     capilar: {
         titulo: "Tratamento Capilar",
         descricao: "Fortalece os fios, combate a queda de cabelo e promove saúde capilar e bem-estar.",
-        imagem: "../img/capilar1.png"
+        imagem: "img/capilar1.png"
     },
 
     labial: {
         titulo: "Preenchimento Labial",
         descricao: "Realça os lábios, melhora o contorno e promove rejuvenescimento com resultados naturais.",
-        imagem: "../img/labial1.png"
+        imagem: "img/labial1.png"
+    },
+
+    limpeza: {
+        titulo: "Limpeza de Pele",
+        descricao: "Procedimento indicado para remover impurezas, controlar a oleosidade e promover renovação celular, deixando a pele mais saudável, limpa e iluminada.",
+        imagem: "img/limpeza-pele.png"
+    },
+
+    microagulhamento: {
+        titulo: "Microagulhamento",
+        descricao: "Técnica que estimula a produção de colágeno e auxilia na melhora da textura da pele, linhas finas e marcas.",
+        imagem: "img/microagulhamento.png"
+    },
+
+    peeling: {
+        titulo: "Peeling Químico",
+        descricao: "Procedimento que promove renovação celular, melhora da textura da pele e auxilia no tratamento de manchas e sinais do envelhecimento.",
+        imagem: "img/peeling-quimico.png"
+    },
+
+    bioestimuladores: {
+        titulo: "Bioestimuladores de Colágeno",
+        descricao: "Tratamento que estimula a produção natural de colágeno, promovendo firmeza, sustentação e rejuvenescimento facial.",
+        imagem: "img/bioestimuladores.png"
+    },
+
+   pdrn: {
+    titulo: "PDRN",
+    descricao: "Tratamento regenerativo que utiliza polinucleotídeos para estimular a renovação celular, melhorar a qualidade da pele, promover hidratação profunda e auxiliar no rejuvenescimento facial.",
+    imagem: "img/pdrn.png"
+},
+
+    skinbooster: {
+        titulo: "Skinbooster",
+        descricao: "Procedimento voltado para hidratação profunda da pele, melhorando elasticidade, brilho e qualidade da pele.",
+        imagem: "img/skinbooster.png"
+    },
+
+    enzimas: {
+        titulo: "Aplicação de Enzimas",
+        descricao: "Procedimento utilizado como auxílio em protocolos personalizados para gordura localizada e definição corporal.",
+        imagem: "img/enzimas.png"
     }
 };
 
